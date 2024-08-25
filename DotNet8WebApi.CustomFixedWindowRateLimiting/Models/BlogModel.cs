107c9567c404638e5401476725889b50011373ba
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-25 10:35:21 +0630

    -

--- DotNet8WebApi.CustomFixedWindowRateLimiting/Models/BlogModel.cs
+++ DotNet8WebApi.CustomFixedWindowRateLimiting/Models/BlogModel.cs
@@ -1,18 +1,17 @@
-﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiting.Models
-{
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
+﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiting.Models;
 
-        public BlogModel(int blogId, string blogTitle, string blogAuthor, string blogContent)
-        {
-            BlogId = blogId;
-            BlogTitle = blogTitle;
-            BlogAuthor = blogAuthor;
-            BlogContent = blogContent;
-        }
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+
+    public BlogModel(int blogId, string blogTitle, string blogAuthor, string blogContent)
+    {
+        BlogId = blogId;
+        BlogTitle = blogTitle;
+        BlogAuthor = blogAuthor;
+        BlogContent = blogContent;
     }
 }
