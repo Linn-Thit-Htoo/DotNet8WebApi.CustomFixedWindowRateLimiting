307d1c39653d41d5c5b7b7d1994801bbac27a1ea
Moe Thet Khine <soniapoudel03@gmail.com>
2024-08-25 11:12:38 +0630

    [GetBlogs]

--- DotNet8WebApi.CustomFixedWindowRateLimiting/Controllers/RateLimitingController.cs
+++ DotNet8WebApi.CustomFixedWindowRateLimiting/Controllers/RateLimitingController.cs
@@ -1,14 +1,18 @@
-﻿namespace DotNet8WebApi.CustomFixedWindowRateLimiting.Controllers;
+﻿using System.Net;
+
+namespace DotNet8WebApi.CustomFixedWindowRateLimiting.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class RateLimitingController : ControllerBase
 {
     private readonly FixedWindowRateLimiter _fixedWindowRateLimiter;
+    private readonly HttpClient _httpClient;
 
-    public RateLimitingController(FixedWindowRateLimiter fixedWindowRateLimiter)
+    public RateLimitingController(FixedWindowRateLimiter fixedWindowRateLimiter, HttpClient httpClient)
     {
         _fixedWindowRateLimiter = fixedWindowRateLimiter;
+        _httpClient = httpClient;
     }
 
     [HttpPost("fixed-window")]
@@ -22,6 +26,19 @@ public class RateLimitingController : ControllerBase
             return StatusCode(429, "Too many requests.");
         }
 
+        return Ok();
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/RateLimiting/fixed-window", null);
+        var responseJson = await response.Content.ReadAsStringAsync();
+        var statusCode = response.StatusCode;
+
+        if(statusCode == HttpStatusCode.TooManyRequests)
+        {
+            return StatusCode(429,responseJson);
+        }
         return Ok();
     }
 }
