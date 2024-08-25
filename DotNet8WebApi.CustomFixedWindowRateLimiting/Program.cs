3efa237b439c2449c30d48ad41746c0421e82a9a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-25 10:34:15 +0630

    -

--- DotNet8WebApi.CustomFixedWindowRateLimiting/Program.cs
+++ DotNet8WebApi.CustomFixedWindowRateLimiting/Program.cs
@@ -2,10 +2,7 @@ using DotNet8WebApi.CustomFixedWindowRateLimiting.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +10,6 @@ builder.Services.AddSingleton(n => new FixedWindowRateLimiter(5, TimeSpan.FromSe
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
